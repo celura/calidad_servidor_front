2444055c4435aa74f45a9d878e9f082350363777
celura <168040193+celura@users.noreply.github.com>
2025-05-25 18:27:41 -0500

    Update RegistroPage.jsx

--- frontend/src/pages/RegistroPage.jsx
+++ frontend/src/pages/RegistroPage.jsx
@@ -58,7 +58,7 @@ const RegisterPage = () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5001/auth/register', {
+      const response = await fetch('https://autenticacionmicroservicio-production.up.railway.app/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
