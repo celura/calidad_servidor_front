923268ef53a0412793a94ca60a8d2d0e0095d3db
celura <168040193+celura@users.noreply.github.com>
2025-05-25 18:27:12 -0500

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -30,7 +30,7 @@ const LoginPage = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5001/auth/login", {
+      const response = await fetch("https://autenticacionmicroservicio-production.up.railway.app/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
